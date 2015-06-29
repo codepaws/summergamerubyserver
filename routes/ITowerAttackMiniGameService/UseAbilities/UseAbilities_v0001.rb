4d590dedebb40ba59acb96342687dc5ad0b73c1c
brodie <brodiesmith@gmail.com>
2015-06-29 23:07:51 +0800

    added web route for client

--- routes/ITowerAttackMiniGameService/UseAbilities/UseAbilities_v0001.rb
+++ routes/ITowerAttackMiniGameService/UseAbilities/UseAbilities_v0001.rb
@@ -1,8 +1,12 @@
 # "POST /ITowerAttackMiniGameService/UseAbilities/v0001/ HTTP/1.1" 404 520 0.0020
 
 post '/ITowerAttackMiniGameService/UseAbilities/v0001/' do 
-  #{"input_json"=>"{\"requested_abilities\":[{\"ability\":2,\"new_lane\":1}],\"gameid\":\"49943\"}","access_token"=>"28d39e6467b5c35a50c8491a57b9aecf","format"=>"protobuf_raw"}
+  #{"input_json"=>"{\"requested_abilities\":[{\"ability\":2,\"new_lane\":1}],\"gameid\":\"49943\"}","access_token"=>"","format"=>"protobuf_raw"}
 
-  abilities = CTowerAttack_UseAbilities_Request.decode(params['input_json'])
-  File.open('stream_log.txt', 'a') { |file| file.write("#{PP.pp(params, '')}\n\n") }
+  request = CTowerAttack_UseAbilities_Request::handle_request(params)
+  reply = CTowerAttack_UseAbilities_Response::create_response(request.gameid, request.requested_abilities)
+
+  #response.headers['Content-Type'] = 'application/octet-stream'
+  #return reply.encode
+  return ''
 end
